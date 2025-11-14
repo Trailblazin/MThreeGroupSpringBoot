9e3a118cf11dddbcf8c7ed017dd00f592c822b51
Seun Osikomaiya <44784626+Trailblazin@users.noreply.github.com>
2025-11-14 11:09:16 +0000

    Created Entites skeleton for DB interaction

---
+++ src/main/java/org/example/entities/Category.java
@@ -0,0 +1,14 @@
+package org.example.entities;
+
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+    private List<Task> tasks = new ArrayList<>();
+}
