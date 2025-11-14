ffe590dce8f93d0900d00f1c64fcf5360e534fbe
Seun Osikomaiya <44784626+Trailblazin@users.noreply.github.com>
2025-11-14 13:12:10 +0000

    Added Get/Set for Task

--- src/main/java/org/example/entities/Task.java
+++ src/main/java/org/example/entities/Task.java
@@ -26,4 +26,60 @@ public class  Task {
     @ManyToOne
     @JoinColumn(name = "category_id")
     private Category category;
+
+    public Priority getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Priority priority) {
+        this.priority = priority;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
 }
