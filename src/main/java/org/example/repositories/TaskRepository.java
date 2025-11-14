64cfa6d5cc3f16ecea05a01ea4be00e30781f474
Seun Osikomaiya <44784626+Trailblazin@users.noreply.github.com>
2025-11-14 13:13:34 +0000

    Added JPARepo definiton to TaskRepo

--- src/main/java/org/example/repositories/TaskRepository.java
+++ src/main/java/org/example/repositories/TaskRepository.java
@@ -1,9 +1,8 @@
 package org.example.repositories;
 
+import org.example.entities.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TaskRepository extends JpaRepository {
-
-}
+public interface TaskRepository extends JpaRepository<Task, Long> {}
